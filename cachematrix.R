eb001de5ad8ca9160d6b9317d23a6dcb801aa57e
MaryLuu <kleopatra069@gmail.com>
2014-06-22 00:05:40 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  # set cached value, NULL because nothing is cached at the beginning
   cache_object <- NULL
-  # 
+  
+  # created matrix with the function. 
   set_martix <- function(y){
     x <<- y 
     cache_object <<- NULL
 }
+# the brand new created matrix
 get_matrix <- function(){
   x
 }
-cache_inv <- function(inv_matrix){
-  cache_object <<- inv_matrix
+# loading and calculation on the new object
+cache_inv <- function(calculation){
+  cache_object <<- calculation
 }
+# get the calculated value
 get_inv <- function(){
   cache_object
 }
-list(set_matrix = set_matrix,get_matrix= get_matrix,
+# list of function
+list(set_matrix = set_matrix, get_matrix= get_matrix,
      cache_inv= cache_inv, get_inv = get_inv)
 }
 
@@ -27,15 +33,21 @@ list(set_matrix = set_matrix,get_matrix= get_matrix,
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  # get the cached value
   cache_object <- x$get_inv()
+  # if the value exists it is given back 
   if(!is.null(cache_object)){
     message("getting cached data")
     return(cache_object)
   }
+  # Otherwise, it calculates the matrix
+  # and sets the value in the cache 
+  # via the setmean function.
   data <- x$get_matrix()
   cache_object <- inv_matrix(data,...)
   x$cache_inv(cache_object)
+  
+  # returning the inversed matrix value
   cache_object
   
 }
